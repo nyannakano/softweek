063bd741e5c93c42a18f410ca571cc33dfeb2427
Gabriel Nakano <gabrielnakano123@gmail.com>
2024-09-22 16:17:25 -0300

    Now connected with back-end to account registering

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,13 +33,30 @@ class RegisteredUserController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password' => ['required', Rules\Password::defaults()],
+            'ra' => 'nullable|string|unique:'.User::class,
+        ], [
+            'ra.unique' => 'O RA informado já está em uso.',
+            'ra.string' => 'O RA informado não é válido.',
+            'ra.nullable' => 'O RA informado não é válido.',
+            'email.unique' => 'O e-mail informado já está em uso.',
+            'email.email' => 'O e-mail informado não é válido.',
+            'email.lowercase' => 'O e-mail informado não é válido.',
+            'email.string' => 'O e-mail informado não é válido.',
+            'email.max' => 'O e-mail informado não é válido.',
+            'name.required' => 'O nome informado não é válido.',
+            'name.string' => 'O nome informado não é válido.',
+            'name.max' => 'O nome informado não é válido.',
+            'password.required' => 'A senha informada não é válida.',
+            'password.string' => 'A senha informada não é válida.',
+            'password.min' => 'A senha informada não é válida.',
         ]);
 
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'ra' => $request->ra,
         ]);
 
         event(new Registered($user));
--- resources/js/Components/RegisterHome.vue
+++ resources/js/Components/RegisterHome.vue
@@ -2,6 +2,8 @@
 const props = defineProps<{
     image_logo: string
 }>();
+
+const csrfToken = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
 </script>
 
 <template>
@@ -10,36 +12,37 @@ const props = defineProps<{
             <img :src="image_logo" alt="Logo Softweek 2024" class="logo-image w-full md:w-1/2 lg:w-1/3 mb-4 md:mb-0">
 
             <div class="max-w-lg w-full">
-                <form class="relative rounded px-8 pt-6 pb-8 mb-4 mt-10 overflow-hidden flex flex-col">
+                <form action="/register" method="POST" class="relative rounded px-8 pt-6 pb-8 mb-4 mt-10 overflow-hidden flex flex-col">
                     <h1 class="text-center text-white text-xl md:text-2xl mb-2">CADASTRE-SE</h1>
+
+                    <input type="hidden" name="_token" :value="csrfToken">
+
                     <div class="mb-4">
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="name"></label>
                         <input
-                            class="shadow appearance-none bg-transparent border-white rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="name" type="text" placeholder="Nome Completo*">
+                            class="shadow appearance-none text-white bg-transparent border-white rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="name" name="name" type="text" placeholder="Nome Completo*">
                     </div>
                     <div class="mb-4">
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="email"></label>
                         <input
-                            class="shadow appearance-none bg-transparent border-white rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="email" type="email" placeholder="Endereço de e-mail*">
+                            class="shadow appearance-none text-white bg-transparent border-white rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="email" name="email" type="email" placeholder="Endereço de e-mail*">
                     </div>
                     <div class="mb-4">
                         <label class="block text-gray-700 text-sm font-bold mb-2" for="ra"></label>
                         <input
-                            class="shadow appearance-none bg-transparent border-white rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            id="ra" type="text" placeholder="R.A.">
+                            class="shadow appearance-none text-white bg-transparent border-white rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                            id="ra" name="ra" type="text" placeholder="R.A.">
                     </div>
                     <div class="mb-6">
-                        <label class="block text-gray-700 text-sm font-bold mb-2" for="password"></label>
+                        <label class="block text-gray-700 text-white text-sm font-bold mb-2" for="password"></label>
                         <input
                             class="shadow appearance-none bg-transparent border-white rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                            id="password" type="password" placeholder="Senha*">
+                            id="password" name="password" type="password" placeholder="Senha*">
                     </div>
                     <div class="flex items-center justify-between">
-                        <button
-                            class="bg-[#600F11] hover:bg-[#120F22] text-white w-full font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                            type="button">
+                        <button class="bg-[#600F11] hover:bg-[#120F22] text-white w-full font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                             CADASTRAR
                         </button>
                     </div>
