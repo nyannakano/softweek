1d330b61f0798539280ddbed3098973a11c9a77f
Gabriel Nakano <gabrielnakano123@gmail.com>
2024-10-01 22:52:47 -0300

    Fixed css

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.ts'],  // Adicione outros arquivos conforme necessário
+            input: [
+                'resources/js/app.ts',
+                'resources/css/app.css'
+            ],
             refresh: true,
         }),
         vue({
