894a4ab31d6e420671fe0b44ffe801e15619a9e2
Gabriel Nakano <palinha@Palinha>
2024-10-07 15:33:44 -0300

    Fixing CPF

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -61,6 +61,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'cpf' => $request->cpf,
             'ra' => $request->ra,
         ]);
 
