222ee5973cdf692e40bc4a2297f6a2ef17c40292
Gabriel Nakano <palinha@Palinha>
2024-10-07 15:39:57 -0300

    Fixing validations

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -30,45 +31,46 @@ class RegisteredUserController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|lowercase|email|max:255|unique:'.User::class,
-            'password' => ['required', Rules\Password::defaults()],
-            'ra' => 'nullable|string|unique:'.User::class,
-            'cpf' => 'required|string|min_digits:11|max_digits:11|unique:'.User::class,
-        ], [
-            'ra.unique' => 'O RA informado já está em uso.',
-            'ra.string' => 'O RA informado não é válido.',
-            'ra.nullable' => 'O RA informado não é válido.',
-            'email.unique' => 'O e-mail informado já está em uso.',
-            'email.email' => 'O e-mail informado não é válido.',
-            'email.lowercase' => 'O e-mail informado não é válido.',
-            'email.string' => 'O e-mail informado não é válido.',
-            'email.max' => 'O e-mail informado não é válido.',
-            'name.required' => 'O nome informado não é válido.',
-            'name.string' => 'O nome informado não é válido.',
-            'name.max' => 'O nome informado não é válido.',
-            'password.required' => 'A senha informada não é válida.',
-            'password.string' => 'A senha informada não é válida.',
-            'password.min' => 'A senha informada não é válida.',
-            'cpf.unique' => 'O CPF informado já está em uso.',
-            'cpf.string' => 'O CPF informado não é válido.',
-            'cpf.min_digits' => 'O CPF informado não é válido.',
-            'cpf.max_digits' => 'O CPF informado não é válido.',
-        ]);
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|lowercase|email|max:255|unique:' . User::class,
+                'password' => ['required', Rules\Password::defaults()],
+                'cpf' => 'required|string|min_digits:11|max_digits:11|unique:' . User::class,
+            ], [
+                'email.unique' => 'O e-mail informado já está em uso.',
+                'email.email' => 'O e-mail informado não é válido.',
+                'email.lowercase' => 'O e-mail informado não é válido.',
+                'email.string' => 'O e-mail informado não é válido.',
+                'email.max' => 'O e-mail informado não é válido.',
+                'name.required' => 'O nome informado não é válido.',
+                'name.string' => 'O nome informado não é válido.',
+                'name.max' => 'O nome informado não é válido.',
+                'password.required' => 'A senha informada não é válida.',
+                'password.string' => 'A senha informada não é válida.',
+                'password.min' => 'A senha informada não é válida.',
+                'cpf.unique' => 'O CPF informado já está em uso.',
+                'cpf.string' => 'O CPF informado não é válido.',
+                'cpf.min_digits' => 'O CPF informado não é válido.',
+                'cpf.max_digits' => 'O CPF informado não é válido.',
+            ]);
 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-            'cpf' => $request->cpf,
-            'ra' => $request->ra,
-        ]);
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'cpf' => $request->cpf,
+                'ra' => $request->ra,
+            ]);
 
-        event(new Registered($user));
+            event(new Registered($user));
 
-        Auth::login($user);
+            Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+            return redirect(route('dashboard', absolute: false));
+        } catch (\Exception $e) {
+            Log::error('Erro: ' . $e);
+            return redirect(route('home', absolute: false))->with('error', 'Erro ao cadastrar usuário.');
+        }
     }
 }
